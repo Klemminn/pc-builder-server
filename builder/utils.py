3e764062e9c10e5db5ac0b4c2b6680eb7896b927
klemminn <klemenz88@gmail.com>
2020-10-15 22:50:39 +0000

    Shrink thumbnail size down to 100px height

--- builder/utils.py
+++ builder/utils.py
@@ -18,7 +18,7 @@ def create_thumbnail(image_file, filename, height):
     return image_buffer
 
 def handle_upload(instance, filename):
-    small_height = 200
+    small_height = 100
     uid = uuid.uuid4()
     file_type = filename.split('.')[-1]
     placement_key = 'components/%s' % (uid,)
