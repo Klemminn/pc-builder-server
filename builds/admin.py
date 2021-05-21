8928e49fc4456892ca4ae46b260da8082dedc4a5
Klemenz Hrafn <klemenz88@gmail.com>
2021-05-21 12:37:40 +0000

    Add build link

--- builds/admin.py
+++ builds/admin.py
@@ -1,8 +1,14 @@
+from django.utils.html import format_html
 from django.contrib import admin
 from .models import Build
 
 @admin.register(Build)
 class BuildAdmin(admin.ModelAdmin):
-    list_display = ('id', 'code', 'offerings', 'created',)
+    list_display = ('id', 'code', 'show_url', 'offerings', 'created',)
     search_fields = ('offerings',)
     readonly_fields = ('created',)
+
+    def show_url(self, obj):
+        return format_html("<a href='https://builder.vaktin.is/build/{code}' target='__blank'>Builder link</a>", code=obj.code)
+
+    show_url.short_description = "Url"
--- config-template.py
+++ config-template.py
@@ -15,15 +15,6 @@ prod = {
     'DEBUG': False
 }
 
-staging = {
-    'SECURE_CONTENT_TYPE_NOSNIFF': True,
-    'SECURE_BROWSER_XSS_FILTER': True,
-    'SESSION_COOKIE_SECURE': True,
-    'CSRF_COOKIE_SECURE': True,
-    'X_FRAME_OPTIONS': 'DENY',
-    'DEBUG': False,
-}
-
 dev = {
     'SECURE_CONTENT_TYPE_NOSNIFF': False,
     'SECURE_BROWSER_XSS_FILTER': False,
