9ec2cfc001e3c1d48a178e738437610a5fb49c53
Klemenz Hrafn <klemenz88@gmail.com>
2021-05-27 22:19:06 +0000

    Fix __str__ on monitors

--- components/models.py
+++ components/models.py
@@ -358,4 +358,4 @@ class Monitor(models.Model):
     offerings = GenericRelation(Offering)
 
     def __str__(self):
-        return '%s. %s - %s' % (self.id, self.name,)
+        return '%s. %s' % (self.id, self.name,)
